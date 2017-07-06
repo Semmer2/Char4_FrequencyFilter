a6b8d4479e4b0d33524ffa5bb1ff9d2383899966
Semmer Smith <semmer@live.cn>
2017-07-06 19:06:11 +0800

    边界填充

--- Char4_FrequencyFilter/Main.cpp
+++ Char4_FrequencyFilter/Main.cpp
@@ -1,11 +1,23 @@
 #include"iostream"
 #include<opencv2/opencv.hpp>
+#include<opencv2/core/core.hpp>//dtfº¯Êý
 
 using namespace std;
 using namespace cv;
 
 int main()
 {
+	Mat image = imread("test.jpg");
 
+	int R = getOptimalDFTSize(image.rows);
+	int C = getOptimalDFTSize(image.cols);
+
+	Mat dst;
+	copyMakeBorder(image, dst, 0, R - image.rows, 0, C - image.cols, BORDER_CONSTANT, Scalar::all(255));
+
+	imshow("Border", dst);
+
+	//dft();
+	waitKey();
 	return 0;
 }
